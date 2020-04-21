6b277929753061b2062535d1097f025bade3c91b
lgarciaos@protonmail.com <lgarciaos@protonmail.com>
2020-04-21 00:23:13 -0500

    proven functionality

--- Wisol.cpp
+++ Wisol.cpp
@@ -15,12 +15,20 @@ Wisol::Wisol(HardwareSerial * serial,int sleep_pin){
 
 void Wisol::begin(){
   _serial->begin(9600); //9600 is a fixed baudrate for wisol modules
+  _delay_ms(200);
+  _serial->println(CMD_CB);
+  _delay_ms(200);
+  read();
+  _serial->println(CMD_TR3);
+  _delay_ms(200);
+  read();
 }
 
 String Wisol::read(){
   char inData[25];
   char rx_buffer;
   byte index = 0;
+  _delay_ms(200);
   if (_serial->available() > 0) {
     while (_serial->available() > 0) {
       rx_buffer = _serial->read();
@@ -36,13 +44,11 @@ String Wisol::get_id(){
   if(_id.equals("")){
     read(); //clean buffer
     _serial->println(CMD_AT);
-    delay(50);
+    _delay_ms(100);
     read(); //should return 'OK'
-    delay(25);
     _serial->println(CMD_ID);// Command to get ID
-    delay(50);
+    _delay_ms(100);
     _id= read();
-    delay(25);
   }
   return _id;
 }
@@ -51,58 +57,85 @@ String Wisol::get_pac(){
   if(_pac.equals("")){
     read(); //clean buffer
     _serial->println(CMD_AT);
-    delay(50);
+    _delay_ms(100);
     read(); //should return 'OK'
-    delay(25);
     _serial->println(CMD_PAC);// Command to get ID
-    delay(50);
+    _delay_ms(100);
     _pac= read();
-    delay(25);
   }
   return _pac;
+}
+
+String Wisol::get_temp(){
+  read(); //clean buffer
+  _serial->println(CMD_AT);
+  _delay_ms(100);
+  read(); //should return 'OK'
+  _serial->println(CMD_TEMP);// Command to get ID
+  _delay_ms(100);
+  return read();
 }
 
 void Wisol::send(String msg){ // takes ~680ms BEWARE
   String wisolResponse = "";
   read(); //clean buffer
-  _serial->println(CMD_AT);
-  delay(50);
+  // Serial.println("AT");
+  _serial->printf(CMD_AT);
+  _delay_ms(200);
   wisolResponse = read();
-  delay(35);
-  _serial->println(CMD_CB);
-  delay(50);
-  wisolResponse = read();
-  delay(35);
+  // Serial.println(wisolResponse);
+
+  // Serial.println("AT$GI?");
   _serial->println(CMD_GI);
-  delay(50);
+  _delay_ms(200);
   wisolResponse = read();
-  delay(50);
+  // Serial.println(wisolResponse);
+
   if (int(wisolResponse[0]) == 48 || int(wisolResponse[2]) < 51) {
+    // Serial.println("AT$RC");
     _serial->println(CMD_RC);
-    delay(50);
-    read();
-    delay(50);
+    _delay_ms(200);
+    wisolResponse = read();
+    // Serial.println(wisolResponse);
   }
-  _serial->print(CMD_WRITE);
-  _serial->println(msg);
-  delay(100);
-  //serial.print(",1");  // uncomment if expecting downlink response
-  read();
-  delay(200);
-  read();
+
+  _serial->println(CMD_WRITE+msg);
+  // Serial.print(CMD_WRITE+msg);
+  //_serial.println(",1");  // uncomment if expecting downlink response
+  _delay_ms(200);
+  // Serial.println("Reading after 200");
+  wisolResponse = read();
+  // Serial.println(wisolResponse);
+  _delay_ms(4000);
+  // Serial.println("Reading after 4000");
+  wisolResponse = read();
+  Serial.println(wisolResponse);
+
+  // Serial.println("AT");
+  _serial->println(CMD_AT);
+  _delay_ms(100);
+  wisolResponse = read();
+  // Serial.println(wisolResponse);
 }
 
 void Wisol::sleep(){
   read(); //clean buffer
   _serial->println(CMD_AT);
-  delay(50);
+  _delay_ms(100);
   read();
-  delay(10);
   _serial->println(CMD_SLEEP); //deep sleep
+}
+
+void Wisol::deep_sleep(){
+  read(); //clean buffer
+  _serial->println(CMD_AT);
+  _delay_ms(100);
+  read();
+  _serial->println(CMD_DEEP_SLEEP); //deep sleep
 }
 
 void Wisol::wakeup(){
   digitalWrite(_sleep_pin,LOW);
-  delay(150);
+  _delay_ms(250);
   digitalWrite(_sleep_pin,HIGH);
 }
--- Wisol.h
+++ Wisol.h
@@ -12,10 +12,14 @@
 #define CMD_ID    "AT$I=10"
 #define CMD_PAC   "AT$I=11"
 #define CMD_CB    "AT$CB=-1,0"
-#define CMD_GI    "AT$GI"
+#define CMD_TR3  "AT$TR=3"
+#define CMD_GI    "AT$GI?"
+#define CMD_TEMP  "AT$T?"
+#define CMD_VOLT  "AT$V?"
 #define CMD_RC    "AT$RC"
 #define CMD_WRITE "AT$SF="
-#define CMD_SLEEP "AT$P=2"
+#define CMD_SLEEP "AT$P=1"
+#define CMD_DEEP_SLEEP "AT$P=2"
 
 class Wisol
 {
@@ -24,8 +28,10 @@ class Wisol
     void begin();
     String get_id(void);
     String get_pac(void);
+    String get_temp(void);
     void send(String msg);
     void sleep(void);
+    void deep_sleep(void);
     void wakeup(void);
   private:
     String read();
